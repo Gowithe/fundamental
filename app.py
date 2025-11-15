e2af83208b70deafc41d7a069cbb6bb58a4245ae
Gowithe <gowitamnuay@gmail.com>
2025-11-15 20:34:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import json
 
 app = Flask(__name__)
 CORS(app)
-
+@app.route('/')
+def home():
+    return render_template('index.html')
 # =====================================================================
 # API KEYS (Never expose to frontend)
 # =====================================================================
